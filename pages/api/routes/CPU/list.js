e9ad4e3869390fc9fdc0c4f7b5f458b5fceba5bc
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-01-29 01:21:16 -0300

    creates find all cpus endpoint

--- pages/api/routes/CPU/list.js
+++ pages/api/routes/CPU/list.js
@@ -1,30 +1,12 @@
-import rawCPUsList from "/pages/api/database/processors.json";
-import paginate from "../../handlers/pagination";
-import sorting,{
-  sortPriceAscending,
-  sortPriceDescending,
-  sortAlphabeticalAscending,
-  sortAlphabeticalDescending,
-} from "../../handlers/sorting";
-import randomizer from "../../handlers/randomizer";
+import {getTables} from "/pages/api/handlers/tables.js"
+import handleRequest from "/pages/api/handlers/request.js"
 
 export default async function handler(req, res) {
   try {
-    let randomized = await randomizer(rawCPUsList);
-    const query = req.query;
-
-    const params = {
-      limit: query.limit ?? 10,
-      page: query.page ?? 1,
-      sortingBy: query.sortingBy ?? "none", //'aphabetical', 'price', 'none'
-      flow: query.flow ?? "descending", // 'descending', 'ascending', 'none'
-    };
-
-    let paginated = await paginate(raw, params.page, params.limit); //data, pageNumber, pageSize
-    let data;
+    let {cpu} = await getTables()
+    const data = await handleRequest(req, cpu)
     res.status(200).json(data);
 
-    
   } catch (err) {
     res.status(500).json({ error: `Internal Server Error: ${err}` });
   }
