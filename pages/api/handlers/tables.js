e21c9b9b3429b3e59eb1bfd8cd9c14f37f447673
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-01-29 01:21:37 -0300

    creates table gathering methods

---
+++ pages/api/handlers/tables.js
@@ -0,0 +1,25 @@
+import cpu from "/pages/api/database/processors.json";
+import cpuFan from "/pages/api/database/cpu_fan.json"
+import cases from "/pages/api/database/cases.json"
+import caseFan from "/pages/api/database/case_fan.json"
+import gpu from "/pages/api/database/gpus.json"
+import keyboard from "/pages/api/database/keyboards.json"
+import motherboard from "/pages/api/database/motherboards.json"
+import mouse from "/pages/api/database/mouse.json"
+import psu from "/pages/api/database/psu.json"
+import ram from "/pages/api/database/ram.json"
+import storage from "/pages/api/database/storage.json"
+
+import randomizer from "./randomizer";
+
+export async function getAllItems(){
+    return [...cpu,...cpuFan,...cases,...caseFan,...gpu,...keyboard,...motherboard,...mouse,...psu,...ram,...storage]
+}
+
+export async function getAllItemsRandomized(){
+    return randomizer(getAllItems())
+}
+
+export async function getTables(){
+    return {cpu,cpuFan,cases,caseFan,gpu,keyboard,motherboard,mouse,psu,ram,storage}
+}
