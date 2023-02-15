f361402040e0f04616ea8ebf27bc1e1298076ffa
Jefferson Willian <jefferson.fechia@gmail.com>
2023-02-15 18:02:28 -0300

    refactor and add cart provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,16 @@
 import Theme from "/pages/src/context/Theme.jsx";
-import Head from 'next/head'
 import Layout from "./src/components/common/layout/Layout";
+import CartProvider from "./src/context/CartContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Theme>
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true" }/>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Abel&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <Layout />
-    <Component {...pageProps} />
-  </Theme>
-  )
+      <CartProvider>
+        <Layout />
+        <Component {...pageProps} />
+      </CartProvider>
+    </Theme>
+  );
 }
 
-export default MyApp
+export default MyApp;
